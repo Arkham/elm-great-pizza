2300592c8bb3e2df928c68e96dab532e8e825ca0
Ju Liu <ju@noredink.com>
2021-03-13 17:14:46 +0000

    Expand one card

--- src/Main.elm
+++ src/Main.elm
@@ -4,14 +4,21 @@ import Browser
 import Element as E exposing (Color, Element)
 import Element.Background as Background
 import Element.Border as Border
+import Element.Events as Events
 import Element.Font as Font
 import Element.Input as Input
 import Html exposing (Html)
 import Html.Attributes
 
 
+type Card
+    = Ingredients
+    | Mixing
+    | Baking
+
+
 type alias Model =
-    {}
+    { expandedCard : Maybe Card }
 
 
 pageTitle : String
@@ -22,7 +29,7 @@ pageTitle =
 main : Program () Model Msg
 main =
     Browser.document
-        { init = \flags -> ( {}, Cmd.none )
+        { init = \flags -> ( { expandedCard = Nothing }, Cmd.none )
         , view =
             \model ->
                 { title = pageTitle
@@ -35,11 +42,6 @@ main =
 
 
 -- Color palette
-
-
-white : Color
-white =
-    E.rgb255 255 255 255
 
 
 black : Color
@@ -73,54 +75,102 @@ orange =
 
 view : Model -> Html Msg
 view model =
-    E.layout [ Background.color champagne, Font.color black ] <|
-        E.column
+    E.layout
+        [ Background.color champagne
+        , Font.color black
+        , E.behindContent <|
+            E.el
+                [ E.width E.fill
+                , E.height E.fill
+                , Events.onClick (ExpandCard Nothing)
+                ]
+                E.none
+        ]
+        (E.column
             [ E.width E.fill
-            , E.padding 30
             ]
-        <|
             [ E.row
                 [ E.width E.fill
                 , E.alignTop
+                , Events.onClick (ExpandCard Nothing)
                 ]
                 [ E.el
-                    [ E.padding 30
+                    [ E.paddingXY 30 60
                     , E.centerX
                     , Font.size 37
                     ]
                     (E.text pageTitle)
                 ]
-            , E.row
-                [ E.width E.fill
-                , E.alignTop
-                , E.spacing 30
-                , E.padding 30
-                ]
-              <|
-                [ card [ Background.color yellow ] "Ingredients"
-                , card [ Background.color pumpkin ] "Mixing & Kneading"
-                , card [ Background.color orange ] "Baking"
-                ]
+            , case model.expandedCard of
+                Just card ->
+                    viewCard card
+
+                Nothing ->
+                    viewHome
             ]
-
-
-card : List (E.Attribute Msg) -> String -> Element Msg
-card attrs title =
-    Input.button
-        ([ Background.color (E.rgb255 230 20 240)
-         , E.padding 30
-         , E.pointer
-         , E.centerX
-         ]
-            ++ attrs
         )
-        { onPress = Nothing
-        , label = E.text title
+
+
+viewHome : Element Msg
+viewHome =
+    E.row
+        [ E.width E.fill
+        , E.alignTop
+        , E.spacing 30
+        , E.paddingXY 30 0
+        ]
+        [ viewButton Ingredients
+        , viewButton Mixing
+        , viewButton Baking
+        ]
+
+
+viewCard : Card -> Element Msg
+viewCard card =
+    E.row [] [ E.el [] (E.text <| cardToString card) ]
+
+
+cardToString : Card -> String
+cardToString card =
+    case card of
+        Ingredients ->
+            "Ingredients"
+
+        Mixing ->
+            "Mixing & Kneading"
+
+        Baking ->
+            "Baking"
+
+
+cardColor : Card -> Color
+cardColor card =
+    case card of
+        Ingredients ->
+            yellow
+
+        Mixing ->
+            pumpkin
+
+        Baking ->
+            orange
+
+
+viewButton : Card -> Element Msg
+viewButton card =
+    Input.button
+        [ Background.color (cardColor card)
+        , E.padding 30
+        , E.centerX
+        ]
+        { onPress = Just (ExpandCard (Just card))
+        , label = E.text (cardToString card)
         }
 
 
 type Msg
     = NoOp
+    | ExpandCard (Maybe Card)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -128,3 +178,6 @@ update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
+
+        ExpandCard card ->
+            ( { model | expandedCard = card }, Cmd.none )
